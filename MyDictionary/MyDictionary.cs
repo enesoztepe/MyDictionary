f87308aaf3acf02cacc234f6fc507988eb5e5cf3
Enes Öztepe <enesoztepebjk@gmail.com>
2021-02-02 14:08:33 +0300

    Ödev 5

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary <TKey,TVal>
+    {
+		List<TKey> keys;
+		List<TVal> vals;
+
+		public MyDictionary()
+		{
+			keys = new List<TKey>();
+			vals = new List<TVal>();
+		}
+		public TVal this[TKey key] 
+		{
+			get
+			{
+				int index = keys.IndexOf(key);
+				TVal value = vals[index];
+				return value;
+			}
+		}
+		public TKey this[TVal val]
+        {
+            get
+            {
+				int index = vals.IndexOf(val);
+				TKey value = keys[index];
+				return value;
+            }
+        }
+		public void Add(TKey key, TVal val)
+		{
+            foreach (var k in keys)
+            {
+				if (k.Equals(key))
+				{
+					throw new ArgumentException();
+				}
+			}
+			keys.Add(key);
+			vals.Add(val);
+		}
+	}
+}
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyDictionary
 {
@@ -6,7 +7,23 @@ namespace MyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyList<int> sayilarList = new MyList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                sayilarList.Add(i);
+            }
+            MyDictionary<int, string> sayilarDictionary = new MyDictionary<int, string>();
+            sayilarDictionary.Add(1, "bir");
+            sayilarDictionary.Add(2, "iki");
+            sayilarDictionary.Add(3, "üç");
+            sayilarDictionary.Add(4, "dört");
+            sayilarDictionary.Add(5, "beş");
+            sayilarDictionary.Add(6, "altı");
+            sayilarDictionary.Add(7, "yedi");
+
+            Console.WriteLine(sayilarDictionary[1]);
+            Console.WriteLine(sayilarDictionary["beş"]);
+
         }
     }
 }
